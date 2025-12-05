c129b03f7424e1047390966eae761d396d6e32c5
tubleranel <jrrtuble@gmail.com>
2025-12-05 20:34:37 +0800

    implement database models

---
+++ lib/mongodb.ts
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+type MongooseCache = {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+};
+
+declare global {
+  var mongoose: MongooseCache | undefined;
+}
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env"
+  );
+}
+
+const cached: MongooseCache = global.mongoose || { conn: null, promise: null };
+
+if (!global.mongoose) {
+  global.mongoose = cached;
+}
+
+async function connectDB(): Promise<typeof mongoose> {
+  //Return exisitng connection if available
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  //Return existing connection promise if one is in progress
+  if (!cached.promise) {
+    const options = {
+      bufferCommands: false, //Disable mongoose buffering
+    };
+
+    //Create new connection promise
+    cached.promise = mongoose
+      .connect(MONGODB_URI!, options)
+      .then((mongoose) => {
+        return mongoose;
+      });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+    throw error;
+  }
+
+  return cached.conn;
+}
+export default connectDB;
